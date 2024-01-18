65e2fefc682a5b3437d33b071400f1a27b2824c1
yuriyaza <yuriy.aza@gmail.com>
2024-01-18 17:56:59 +0200

    Add migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,18 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Provider\Lorem;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CategoriesSeeder::class,
+            PostsSeeder::class,
+            CommentsSeeder::class,
+        ]);
     }
 }
